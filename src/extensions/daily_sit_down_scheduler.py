5d6a23349ec141d34513f45cccc8d878baba2dda
Erik Kristofer Anderson <52926724+ekand@users.noreply.github.com>
2023-08-15 10:00:09 -0500

    Update and rename meeting_scheduler.py to daily_sit_down_scheduler.py
    Includes a docstring with directions for future development.

---
+++ src/extensions/daily_sit_down_scheduler.py
@@ -0,0 +1,8 @@
+"""Schedules Sit-Down Meetings
+
+Will schedule a 'Sit-Down Meeting' at 16:00 UTC each weekday in a server,
+if the server opts in with slash command from a server member with the
+"manage events" permission.
+The events will have a one hour duration and will be created 36 hours before 
+the event start time.
+"""
